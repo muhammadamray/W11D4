0d2649b0f7e8edd8a280bff8aaf185a083fc5a4d
Muhammad Amray <muhammadamray@gmail.com>
2023-09-07 14:40:52 -0400

    doing pokemon model

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
@@ -14,4 +14,38 @@
 #  updated_at :datetime         not null
 #
 class Pokemon < ApplicationRecord
+    TYPES = [
+        'fire',
+        'electric',
+        'normal',
+        'ghost',
+        'psychic',
+        'water',
+        'bug',
+        'dragon',
+        'grass',
+        'fighting',
+        'ice',
+        'flying',
+        'poison',
+        'ground',
+        'rock',
+        'steel'
+    ].sort.freeze
+
+    validates :number, :name, :attack, :defense, :poke_type, :image_url, presence: true 
+    validates :captured, inclusion: [true, false]
+    validates :name, length: { in: 3..255 }, uniqueness: { message: "'%{value}' is already in use" }
+    validates :number, numericality: {minimum: 1}, uniqueness: {message: "'%{value}' is outside of range"}
+    validates :attack, numericality: {in: 0..100}
+    validates :defense, numericality: {in: 0..100}
+
+    validates :poke_type, inclusion: { in: TYPES, message: "'%{value}' is not a valid Pokemon type" }
+
+
+
+
+
+
+
 end
